93a1ed7002ca75809509ec6fa2bf3632aade1fc2
asmaamohameed <asmaa.mo104@gmail.com>
2025-04-23 13:11:09 +0200

    SCANDIWEB-FULLSTACK-TASK-04: Fix(GraphQL) - (editing queries.ts file)

--- Frontend/src/GraphQL/clients.ts
+++ Frontend/src/GraphQL/clients.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:8080/graphql",
+  uri: import.meta.env.VITE_API_URL,
 });
 
 const client = new ApolloClient({
