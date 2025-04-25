d4153bfd8ec0348e387503c78304309c694c5df1
asmaamohameed <asmaa.mo104@gmail.com>
2025-04-25 12:27:09 +0300

    SCANDIWEB-FULLSTACK-TASK-04: Feature(index.php) - (add index.php)

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
+require_once '../vendor/autoload.php';
+require_once '../app/Routes/web.php';
+require_once '../src/Support/helper.php';
+use Dotenv\Dotenv;
+use Scandiweb\Application;
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(base_path()); 
+$dotenv->load();
+
+$app = new Application();
+
+$app->run();
