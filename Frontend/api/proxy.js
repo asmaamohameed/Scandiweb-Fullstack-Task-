7a6cb1322b4b470634c19355a6725fad6c574863
asmaamohameed <asmaa.mo104@gmail.com>
2025-04-28 19:48:20 +0300

    Edit clintes.js

--- Frontend/api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    const response = await fetch('https://ecommerce-task.atwebpages.com/graphql', {
-      method: req.method,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(req.body)
-    });
-    const data = await response.json();
-    res.status(200).json(data);
-  }
