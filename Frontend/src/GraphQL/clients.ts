b98027fb162cdd57426b91a57d917e11a028121e
asmaamohameed <asmaa.mo104@gmail.com>
2025-05-03 18:29:25 +0300

    SCANDIWEB-FULLSTACK-TASK-05: Fix(Router) - (Edit rotuer file)

--- Frontend/src/GraphQL/clients.ts
+++ Frontend/src/GraphQL/clients.ts
@@ -1,12 +1,32 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
 
 // Set the GraphQL endpoint based on the environment
 const graphqlEndpoint = import.meta.env.PROD
   ? "https://sh1r3f.com/graphql"
   : "http://localhost:8080/graphql";
 
+  const httpLink = new HttpLink({ uri: graphqlEndpoint });
+
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: graphqlEndpoint }),
+  link: ApolloLink.from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
