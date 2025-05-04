9bac7f7b08163f8c45807d33e13fdab0d879ffe7
asmaamohameed <asmaa.mo104@gmail.com>
2025-05-04 14:25:59 +0300

    SCANDIWEB-FULLSTACK-TASK-05: Fix(Router) - (Edit rotuer)

--- Frontend/src/Components/UI/Navbar.tsx
+++ Frontend/src/Components/UI/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_CATEGORIES } from "../../GraphQL/queries";
 import Loading from "../Fallbacks/Loading";
@@ -8,39 +8,35 @@ interface Category {
 }
 
 const Nav: React.FC = () => {
+  const location = useLocation();
   const { loading, error, data } = useQuery(GET_CATEGORIES);
-  const isActive = (path: string) => location.pathname === path;
-  
+
   if (loading) return <Loading />;
   if (error)
-    return (
-      <nav className="p-4 text-red-700">
-        Categories failed to load.
-      </nav>
-    );
+    return <nav className="p-4 text-red-700">Categories failed to load.</nav>;
 
   return (
     <nav className="relative">
       <div className="hidden md:flex gap-6">
-        {data.categories.map((category: Category) => (
-          <NavLink
-            key={category.name}
-            to={`/${category.name.toLowerCase()}`}
-            data-testid={isActive(`/${category.name.toLowerCase()}`) ? "active-category-link" : "category-link"}
-            className={({ isActive }) => {
-              const baseClass =
-                "relative pb-2 after:absolute after:left-0 after:bottom-0 after:h-[2px] after:transition-all after:duration-300";
-              const activeStyles =
-                "text-green-500 after:w-full after:bg-green-500";
-              const inactiveStyles = "after:w-0 after:bg-transparent";
+        {data.categories.map((category: Category) => {
+          const path = `/${category.name.toLowerCase()}`;
+          const isActive = location.pathname === path;
 
-              return `${baseClass} ${isActive ? activeStyles : inactiveStyles}`;
-            }}
-          >
-                {category.name.toUpperCase()}
-              
-          </NavLink>
-        ))}
+          return (
+            <Link
+              key={category.name}
+              to={path}
+              data-testid={isActive ? "active-category-link" : "category-link"}
+              className={`relative pb-2 ${
+                isActive
+                  ? "text-green-500 after:w-full after:bg-green-500"
+                  : "after:w-0 after:bg-transparent"
+              } after:absolute after:left-0 after:bottom-0 after:h-[2px] after:transition-all after:duration-300`}
+            >
+              {category.name.toUpperCase()}
+            </Link>
+          );
+        })}
       </div>
     </nav>
   );
