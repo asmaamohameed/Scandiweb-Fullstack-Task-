4ceda2266c0b7f8e213fe8965de56b1b84492120
asmaamohameed <43033428+asmaamohameed@users.noreply.github.com>
2025-05-09 16:56:52 +0300

    Update clients.ts

--- Frontend/src/GraphQL/clients.ts
+++ Frontend/src/GraphQL/clients.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-  // link: new HttpLink({ uri: "https://sh1r3f.com/graphql" }),
-  link: new HttpLink({ uri: "http://localhost:8080/graphql" }),
+  link: new HttpLink({ uri: "https://sh1r3f.com/graphql" }),
   cache: new InMemoryCache(),
 });
 
